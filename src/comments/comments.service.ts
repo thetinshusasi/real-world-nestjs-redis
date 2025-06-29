97b22b63d215e8c24f7c11510bd9d48f7fd4b48e
thetinshusasi <thetinshusasi@gmail.com>
2025-06-29 03:42:13 -0300

    feat(app): Ticket 29 June 2025 - 3 Added Comments Module
    Added Code for comments and post service and module

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,26 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { UpdateCommentDto } from "./dto/update-comment.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Comment } from "./entities/comment.entity";
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
+  ) { }
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    return this.commentRepository.save(createCommentDto);
   }
 
   findAll() {
-    return `This action returns all comments`;
+    return this.commentRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} comment`;
+    return this.commentRepository.findOne({ where: { id: id.toString() } });
   }
 
   update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+    return this.commentRepository.update(id, updateCommentDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} comment`;
+    return this.commentRepository.delete(id);
   }
 }
--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,24 +1,30 @@
-import { Post } from 'src/posts/entities/post.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Post } from "src/posts/entities/post.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
 
-@Entity({ name: 'comments' })
+@Entity({ name: "comments" })
 export class Comment {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({ type: 'varchar' })
-    content: string;
+  @Column({ type: "varchar" })
+  content: string;
 
-    @Column({ type: 'varchar' })
-    createdAt: string;
+  @Column({ type: "bigint" })
+  createdAt: number;
 
-    @Column({ type: 'varchar' })
-    updatedAt: string;
+  @Column({ type: "bigint" })
+  updatedAt: number;
 
-    @Column({ type: 'uuid' })
-    postId: string;
+  @Column({ type: "uuid" })
+  postId: string;
 
-    @ManyToOne(() => Post, (post) => post.comments)
-    @JoinColumn({ name: 'postId' })
-    post: Post;
+  @ManyToOne(() => Post, (post) => post.comments)
+  @JoinColumn({ name: "postId" })
+  post: Post;
 }
