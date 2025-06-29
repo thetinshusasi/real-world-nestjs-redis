e66530184f022f11c6ed148bf68323760e44d948
thetinshusasi <thetinshusasi@gmail.com>
2025-06-29 03:40:05 -0300

    fix(app): Ticket 29 June 2025 Fix : ForRootAsync Issue fix
    Nest app was not running . Fixed configuration  of TypeORM.forRootAsync

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,43 @@
-import { Inject, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SharedModule } from './shared/shared.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommentsModule } from './comments/comments.module';
-import { PostsModule } from './posts/posts.module';
+import { Inject, Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { SharedModule } from "./shared/shared.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CommentsModule } from "./comments/comments.module";
+import { PostsModule } from "./posts/posts.module";
+
+import * as path from 'path';
+import { DataSource, DataSourceOptions } from "typeorm";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: `postgres`,
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        migrations: ['src/migrations/*.ts'],
-        migrationsTableName: 'migrations',
-        // TODO: remove this in production
-        // synchronize: true,
-      }),
+      useFactory: async (configService: ConfigService) => {
+
+        return {
+          type: "postgres",
+          host: configService.get("DB_HOST"),
+          port: Number(configService.get("DB_PORT")),
+          username: configService.get("DB_USERNAME"),
+          password: configService.get("DB_PASSWORD"),
+          database: configService.get("DB_NAME"),
+          entities: [path.resolve(__dirname, '**/entities/*.entity{.ts,.js}')],
+          migrations: [path.resolve(__dirname, 'migrations/*.ts')],
+          migrationsTableName: "migrations",
+          // TODO: remove this in production, keep this true for development
+          synchronize: false,
+        }
+      },
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options as DataSourceOptions).initialize();
+        return dataSource;
+      },
     }),
     SharedModule,
     CommentsModule,
